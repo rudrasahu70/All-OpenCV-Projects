67ecb302933c3ac8ba9e7cceca6dcb7f6ad17776
rudrasahu70 <rudrasahu717@gmail.com>
2025-06-16 18:21:25 +0530

    Update detect_mask_image.py

--- Face Mask Detection/detect_mask_image.py
+++ Face Mask Detection/detect_mask_image.py
@@ -1,7 +1,7 @@
 # USAGE
 # python detect_mask_image.py --image examples/example_01.png
 
-# import the necessary packages
+# import the necessary packages    
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
